772ec598aca47551d10368c521e022c4c921060d
willowmoon <willowmoon@pop-os.localdomain>
2020-05-10 16:26:23 -0400

    simple routing

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const blog = () => {
+  return (
+    <div>
+      hello from blog page<Link to="/">Home Page</Link>
+    </div>
+  )
+}
+
+export default blog
