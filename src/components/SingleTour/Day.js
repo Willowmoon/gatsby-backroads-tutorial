890275497514c7d1480fb33c92a53dec33af898e
willowmoon <willowmoon@pop-os.localdomain>
2020-05-11 12:02:09 -0400

    daily schedule set

--- src/components/SingleTour/Day.js
+++ src/components/SingleTour/Day.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from "react"
+import { FaAngleDown } from "react-icons/fa"
+import styles from "../../css/day.module.css"
 
-const Day = () => {
-    return (
-        <div>
-            this is day 1
-        </div>
-    )
+const Day = ({ day, info }) => {
+  const [isShown, setIsShown] = useState(false)
+
+  const toggleDay = () => setIsShown(isShown => !isShown)
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}{" "}
+        <button onClick={toggleDay} className={styles.btn}>
+          <FaAngleDown />
+        </button>
+      </h4>
+      {isShown && <p>{info}</p>}
+    </article>
+  )
 }
 
 export default Day
